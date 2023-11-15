855930b7351115d590ede475e5957513b173b8dd
usamaserag <usamaserageldeen@gmail.com>
2023-11-15 02:38:20 +0200

    Change design for signup page

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Button, Form, Input } from "antd";
+import { Form, Input } from "antd";
 import { Link } from "react-router-dom";
 import firebase from "../firebase";
+import { FaUserLock } from "react-icons/fa";
 
 const ForgetPassword = () => {
   const [email, setEmail] = useState("");
@@ -23,53 +24,66 @@ const ForgetPassword = () => {
     setEmail(value);
   };
   return (
-    <div className="p-3">
-      {!isEmailSent ? (
-        <>
-          <div className="navbar_logo text-center">Serag</div>
-          <h2 className="text-xl text-center font-bold text-2xl">
-            Enter your email address to reset your password.
-          </h2>
-          <Form
-            name="normal_login"
-            className="md:w-6/12 w-full my-0 mx-auto flex flex-col gap-2 p-3"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={handleSendEmail}
-          >
-            <Form.Item
-              onChange={handleEmailChange}
-              value={email}
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your E-mail!",
-                },
-              ]}
-            >
-              <Input placeholder="Enter your email" />
-            </Form.Item>
-            <div className="d-flex flex-col gap-2">
-              <Form.Item>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className="bg-primary mb-0 w-full"
-                >
-                  Send
-                </Button>
-              </Form.Item>
-              <Link to="/login">
-                <Button className="w-full">Cancel</Button>
-              </Link>
+    <div className="flex items-center justify-center h-screen bg-emerald-100">
+      <div className="w-full flex flex-col gap-2 p-5 pt-14 md:w-1/2 shadow-xl rounded-xl m-5 bg-slate-50 relative">
+        {!isEmailSent ? (
+          <>
+            <div className="w-24 h-24 flex items-center justify-center rounded-full bg-slate-50 absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <div className="shadow-currentShadow flex items-center justify-center w-20 h-20 rounded-full bg-emerald-100">
+                <FaUserLock className="text-xl text-primary" />
+              </div>
             </div>
-          </Form>
-        </>
-      ) : (
-        <p>Password reset email sent. Please check your email inbox.</p>
-      )}
+            <div className="navbar_logo text-center">Serag</div>
+            <h2 className="text-center text-l font-medium">
+              Enter your email address to reset your password.
+            </h2>
+            <Form
+              name="normal_login"
+              className="w-full flex flex-col gap-2"
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={handleSendEmail}
+            >
+              <Form.Item
+                onChange={handleEmailChange}
+                value={email}
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your E-mail!",
+                  },
+                ]}
+              >
+                <Input
+                  placeholder="Enter your email"
+                  className="border border-gray-300 focus:outline-none focus:border-primary rounded-3xl bg-white py-1.5 px-2 mt-1 text-gray-900 placeholder:text-xs placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+                />
+              </Form.Item>
+
+              <button
+                type="primary"
+                htmlType="submit"
+                className="rounded-3xl bg-primary p-2 text-white cursor-pointer transition-all duration-200 hover:bg-primaryHover"
+              >
+                Send
+              </button>
+              <p className="flex items-center justify-center gap-1">
+                <span>Don't have an account</span>
+                <Link
+                  to="/signup"
+                  className="text-primary cursor-pointer hover:underline hover:text-primary"
+                >
+                  Sign up
+                </Link>
+              </p>
+            </Form>
+          </>
+        ) : (
+          <p>Password reset email sent. Please check your email inbox.</p>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import Button from "../components/Button";
 import Loading from "../components/Loading";
 import firebase from "../firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { imageDb } from "../firebase";
+import { FaUserPlus } from "react-icons/fa";
 
 const SignupPage = () => {
   const navigate = useNavigate();
@@ -101,117 +101,129 @@ const SignupPage = () => {
   return isSignUp ? (
     <Loading />
   ) : (
-    <div className="md:w-6/12 w-full my-0 mx-auto flex flex-col gap-2 p-3">
-      <div className="navbar_logo text-center">Serag</div>
-      <h3 className="text-xl text-center font-bold text-2xl">
-        Sign Up To Serag Store
-      </h3>
-      <form
-        className="w-full flex flex-col gap-2"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="flex flex-col">
-          <label className="text-gray-700" htmlFor="userName">
-            User Name
-          </label>
-          <input
-            id="userName"
-            type="text"
-            placeholder="Enter your user name ..."
-            className="border-0 bg-white py-1.5 pl-1 mt-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-            {...register("userName", {
-              required: "User name is required",
-              maxLength: {
-                value: 20,
-                message: "Maximum length is 20 characters",
-              },
-            })}
-          />
-          {errors.userName && (
-            <small className="text-red-500">{errors.userName.message}</small>
-          )}
+    <div className="flex items-center justify-center h-screen bg-emerald-100">
+      <div className="w-full flex flex-col gap-2 p-5 pt-14 md:w-1/2 shadow-xl rounded-xl m-5 bg-slate-50 relative">
+      <div className="w-24 h-24 flex items-center justify-center rounded-full bg-slate-50 absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="shadow-currentShadow flex items-center justify-center w-20 h-20 rounded-full bg-emerald-100">
+            <FaUserPlus className="text-xl text-primary" />
+          </div>
         </div>
-        <div className="flex flex-col">
-          <label className="text-gray-700" htmlFor="email">
-            E-mail
-          </label>
-          <input
-            id="email"
-            className="border-0 bg-white py-1.5 pl-1 mt-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-            type="email"
-            placeholder="Enter your email ..."
-            {...register("email", {
-              required: "Email Address is required",
-              validate: (value) => {
-                if (emailIsUse) {
-                  // If emailIsUse error is present, clear it when the input is valid
-                  clearEmailError();
+        <div className="navbar_logo text-center">Serag</div>
+        <h3 className="text-center text-xl font-medium">
+          Sign Up To Serag Store
+        </h3>
+        <form
+          className="w-full flex flex-col gap-2"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col">
+            <label className="text-gray-600 text-sm" htmlFor="userName">
+              User Name
+            </label>
+            <input
+              id="userName"
+              type="text"
+              placeholder="Enter your user name ..."
+              className="border border-gray-300 focus:outline-none focus:border-primary rounded-3xl bg-white py-1.5 px-2 mt-1 text-gray-900 placeholder:text-xs placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+              {...register("userName", {
+                required: "User name is required",
+                maxLength: {
+                  value: 20,
+                  message: "Maximum length is 20 characters",
+                },
+              })}
+            />
+            {errors.userName && (
+              <small className="text-red-500">{errors.userName.message}</small>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label className="text-gray-600 text-sm" htmlFor="email">
+              E-mail
+            </label>
+            <input
+              id="email"
+              className="border border-gray-300 focus:outline-none focus:border-primary rounded-3xl bg-white py-1.5 px-2 mt-1 text-gray-900 placeholder:text-xs placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+              type="email"
+              placeholder="Enter your email ..."
+              {...register("email", {
+                required: "Email Address is required",
+                validate: (value) => {
+                  if (emailIsUse) {
+                    // If emailIsUse error is present, clear it when the input is valid
+                    clearEmailError();
+                    return true;
+                  }
                   return true;
-                }
-                return true;
-              },
-            })}
-          />
-          {errors.email && (
-            <small className="text-red-500">{errors.email.message}</small>
-          )}
-          {emailIsUse && <small className="text-red-500">{emailIsUse}</small>}
-        </div>
-        <div className="flex flex-col">
-          <label>Password</label>
+                },
+              })}
+            />
+            {errors.email && (
+              <small className="text-red-500">{errors.email.message}</small>
+            )}
+            {emailIsUse && <small className="text-red-500">{emailIsUse}</small>}
+          </div>
+          <div className="flex flex-col">
+            <label className="text-gray-600 text-sm">Password</label>
+            <input
+              type="password"
+              placeholder="At least 6 characters"
+              className="border border-gray-300 focus:outline-none focus:border-primary rounded-3xl bg-white py-1.5 px-2 mt-1 text-gray-900 placeholder:text-xs placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              })}
+            />
+            {errors.password && (
+              <small className="text-red-500">{errors.password.message}</small>
+            )}
+          </div>
+
+          <div className="flex flex-col">
+            <label className="text-gray-600 text-sm">Re-enter Password</label>
+            <input
+              type="password"
+              placeholder="confirm your password"
+              className="border border-gray-300 focus:outline-none focus:border-primary rounded-3xl bg-white py-1.5 px-2 mt-1 text-gray-900 placeholder:text-xs placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+              {...register("confirmPassword", {
+                required: "Confirm Password is required",
+                validate: (value) =>
+                  value === password || "Passwords do not match",
+              })}
+            />
+            {errors.confirmPassword && (
+              <small className="text-red-500">
+                {errors.confirmPassword.message}
+              </small>
+            )}
+          </div>
+          {/* input to upload image it is optional */}
+
+          {/* <div className="flex flex-col">
+            <label className="text-gray-600 text-sm">upload Your Image</label>
+            <input
+              type="file"
+              className="file-input file-input-bordered file-input-sm w-full max-w-xs"
+              onChange={(e) => setImage(e.target.files[0])}
+            />
+          </div> */}
+
           <input
-            type="password"
-            placeholder="At least 6 characters"
-            className="border-0 bg-white py-1.5 pl-1 mt-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 6,
-                message: "Password must be at least 6 characters",
-              },
-            })}
+            className="rounded-3xl mt-2 bg-primary p-2 text-white cursor-pointer transition-all duration-200 hover:bg-primaryHover"
+            type="submit"
+            value="Create account"
           />
-          {errors.password && (
-            <small className="text-red-500">{errors.password.message}</small>
-          )}
-        </div>
-
-        <div className="flex flex-col">
-          <label>Re-enter Password</label>
-          <input
-            type="password"
-            placeholder="confirm your password"
-            className="border-0 bg-white py-1.5 pl-1 mt-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-            {...register("confirmPassword", {
-              required: "Confirm Password is required",
-              validate: (value) =>
-                value === password || "Passwords do not match",
-            })}
-          />
-          {errors.confirmPassword && (
-            <small className="text-red-500">
-              {errors.confirmPassword.message}
-            </small>
-          )}
-        </div>
-        {/* input to upload image it is optional */}
-
-        <div className="flex flex-col">
-          <label>upload Your Image</label>
-          <input type="file" className="file-input w-full max-w-xs bg-primary file-input-success text-white" onChange={(e) => setImage(e.target.files[0])} />
-        </div>
-
-        <input
-          className="rounded-lg bg-primary px-2.5 py-1.5 text-white cursor-pointer transition-all duration-200 hover:bg-primaryHover"
-          type="submit"
-          value="Create account"
-        />
-      </form>
-      <Button
-        text="I already have account"
-        handleClick={redirectToLogin}
-        btn_class="w-full text-primary text-center rounded-lg border border-primary bg-white px-2.5 py-1.5 cursor-pointer"
-      />
+        </form>
+        <button
+          onClick={redirectToLogin}
+          className="text-primary text-sm text-center cursor-pointer hover:underline"
+        >
+          I already have account
+        </button>
+      </div>
     </div>
   );
 };
